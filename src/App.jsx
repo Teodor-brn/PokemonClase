7381fca1431ab9205e35a8e8d77fca6f5e758483
Teodor-brn <alumno.1130246@ies-azarquiel.es>
2025-09-26 12:52:04 +0200

    Primer

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+import PokemonCard from '../Components/PokemonCard';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon-species?limit=1025')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setPokemons(data.results);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
+  return (
+    <div className="row">
+      {pokemons.map((pokemon) => (
+         
+        <div key={pokemon.name} className="col-md-3 col-lg-2 mb-1"> 
+        <div className="p-1 mt-3 border bg-light">       
+            <PokemonCard name={pokemon.name} url={pokemon.url} />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
